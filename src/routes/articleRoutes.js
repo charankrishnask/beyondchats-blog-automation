e5065a70962441a17767a68e7b720967ac1d2fd4
Charan S K <charankrishna9104.com>
2025-12-29 21:37:22 +0530

    feat: add CRUD APIs for articles

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/articleController");
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Article route working' });
-});
+router.post("/", controller.createArticle);
+router.get("/", controller.getArticles);
+router.get("/:id", controller.getArticleById);
+router.put("/:id", controller.updateArticle);
+router.delete("/:id", controller.deleteArticle);
 
 module.exports = router;
